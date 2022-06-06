b60342a44981878ec2ae594b2517a239d4cb2ff2
Abhishek <abhishektyagihere@gmail.com>
2022-06-06 18:11:02 +0530

    06-06-2022

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const router = express.Router();
-
-router.get('/',(req,res)=>{
-     obj = {
-         name : "Abhishek" ,
-         age : 18 
-     }
-     res.json(obj);
+const { body, validationResult } = require('express-validator');
+const notes = require('../models/Notes');
+const fetchuser = require('../middleware/fetchuser');
+// Fetch all of the user notes 
+router.get('/fetchallnotes', fetchuser, async (req, res) => {
+    const note = await notes.find({ user: req.user.id });
+    res.json(note);
+})
+// Add new note
+router.post('/addnote', fetchuser, [
+    body('title', 'Please enter a valid email').isLength({ min: 3 }),
+    body('description', 'Please enter a valid name').isLength({ min: 10 }),
+], async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
 })
 module.exports = router 
